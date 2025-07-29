c9f8087c90c1425134d734d2e385665b51c81d39
Saltina <saltinaenquiries@gmail.com>
2025-07-29 17:36:47 +0100

    fix: (TLNEAD-21) migration: users

---
+++ database/migrations/1753806454546_create_create_users_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('full_name').nullable()
+      table.string('email').notNullable().unique()
+      table.string('password').notNullable()
+      table.string('remember_me_token').nullable()
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).nullable()
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
